a91e37b0452377379ac0c5589b3f4324217d448a
Pilou <pierre-louis.franques@epitech.eu>
2020-01-23 16:46:09 +0100

    Menu + sound/music menu

---
+++ Assets/Pilou/Musics-Sounds/PlaySoundsOnObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlaySoundsOnObject : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource OnMouseOverSound;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnMouseOverSound.Play();
+    }
+}
---
+++ Assets/Pilou/Script/InterractionMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterractionMenu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject DeckMenu;
+
+
+    
+    public void OnStartButton()
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnDeckMenuButton()
+    {
+        MainMenu.SetActive(false);
+        DeckMenu.SetActive(true);
+    }
+
+    public void OnBackButton()
+    {
+        DeckMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void OnQuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
