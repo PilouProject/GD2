eb87c8310298f637c82ac92a179e7806a13428b8
Pilou <pierre-louis.franques@epitech.eu>
2020-01-24 21:09:07 +0100

    Add fonction: TakeDmg, Heal, Add/Supp super stack + HUD Life/super bar

---
+++ Assets/Pilou/LifeBar-SuperBar/LifeHUD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeHUD : MonoBehaviour
+{
+    private float _lifeSliderMaxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lifeSliderMaxValue = GetComponent<Slider>().maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //fonction to deal damage to a player, return false if he dies or true if he still lives
+    public bool TakeDamage(int _nbDamage)
+    {
+        GetComponent<Slider>().value = GetComponent<Slider>().value + _nbDamage;
+        if (GetComponent<Slider>().value >= _lifeSliderMaxValue)
+            return (false);
+        return (true);
+    }
+
+    //fonction to heal the player
+    public void Heal(int _nbHeal)
+    {
+        if (GetComponent<Slider>().value - _nbHeal < 0)
+            GetComponent<Slider>().value = 0;
+        else
+            GetComponent<Slider>().value = GetComponent<Slider>().value - _nbHeal;
+    }
+}
---
+++ Assets/Pilou/LifeBar-SuperBar/SuperHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuperHUD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //function to add or supp a nb of stack of super attack, return false if it's time to "NIQUER DES MERES" 
+    public bool AddSuppSuperBar(int _nb)
+    {
+        GetComponent<Slider>().value = GetComponent<Slider>().value + _nb;
+        if (GetComponent<Slider>().value < 0)
+            GetComponent<Slider>().value = 0;
+        else if (GetComponent<Slider>().value >= GetComponent<Slider>().maxValue)
+            return (false);
+        return (true);
+    }
+}
