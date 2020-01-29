733263ab7d016146100469f693c4535d61722e85
najib-k <najib1.kardjoudj@epitech.eu>
2020-01-29 01:31:21 +0100

    Characters animation

---
+++ Assets/Naj/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    public GameObject Player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireAttack()
+    {
+        Animator m_animator = Player.GetComponent<Animator>();
+        m_animator.SetTrigger("attack");
+    }
+
+    public void FireSpecial()
+    {
+        Animator m_animator = Player.GetComponent<Animator>();
+        m_animator.SetTrigger("special");
+    }
+
+    public void FireDeath()
+    {
+        Animator m_animator = Player.GetComponent<Animator>();
+        m_animator.SetBool("death", true);
+    }
+}
