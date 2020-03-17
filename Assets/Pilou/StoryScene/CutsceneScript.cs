8de600fce264172487d90c2c418a7147167a011a
GemsKnight <adrien.lenoir@epitech>
2020-03-17 11:59:23 +0100

    story mode
    déplacement ryu + début dialog

---
+++ Assets/Pilou/StoryScene/CutsceneScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (test());
+    }
+
+    IEnumerator test(){
+        yield return new WaitForSeconds (5.0f);
+        Debug.Log("ezaeaz");
+        SceneManager.LoadScene("Assets/Pilou/FinalScene/FinalScene.unity");
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
