9d0c97dd242c4bd926fa242ece05967115bbff5a
GemsKnight <adrien.lenoir@epitech>
2020-03-17 12:56:15 +0100

    animation story

--- Assets/Pilou/StoryScene/CutsceneScript.cs
+++ Assets/Pilou/StoryScene/CutsceneScript.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CutsceneScript : MonoBehaviour
 {
+    public GameObject Ryu;
+    public GameObject Ken;
+    public Text RyuText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,12 @@ public class CutsceneScript : MonoBehaviour
     IEnumerator test(){
         yield return new WaitForSeconds (5.0f);
         Debug.Log("ezaeaz");
+        RyuText.text = "OMG";
+        Debug.Log(RyuText.text);
+        Ryu.GetComponent<PlayerAnimationController>().FireDefense();
+        Ken.GetComponent<PlayerAnimationController>().FireSpecial();
+        //Ryu.Play("attack");
+        yield return new WaitForSeconds (5.0f);
         SceneManager.LoadScene("Assets/Pilou/FinalScene/FinalScene.unity");
         
     }
